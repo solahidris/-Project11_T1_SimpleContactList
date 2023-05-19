1b25e46e4c3d6b32b7f34faa46223c85ef7d0a85
solahidris <emailsolah@gmail.com>
2023-05-19 22:28:31 +0800

    added tailwind css and app description

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./images/logo.svg";
+import "./App.css";
+import Header from "./components/header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-slate-800/90 h-screen">
+      <div className="flex justify-center pt-10">
+        <img src={logo} className="h-20 animate-bounce" alt="logo" />
+      </div>
+      <Header />
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/App.js", "./src/components/header.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
