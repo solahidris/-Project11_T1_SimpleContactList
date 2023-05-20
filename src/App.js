04f21ad1ffc30938cbe0bb1e03071e6c96283bd7
solahidris <emailsolah@gmail.com>
2023-05-20 23:25:57 +0800

    displayed selected api info as wanted

--- src/App.js
+++ src/App.js
@@ -1,32 +1,83 @@
-// import logo from "./images/logo.svg";
 import "./App.css";
 import HeaderApp from "./components/HeaderApp";
+import React from "react";
+import { useState, useEffect } from "react";
 
 function App() {
+  // normal array mapping (delete later)
+  const contactList = [
+    { fullName: "user1", age: 11 },
+    { fullName: "user2", age: 22 },
+    { fullName: "user3", age: 33 },
+  ];
 
-  const contactList = [{fullName:"user1", age:11}, {fullName:"user2", age:22}, {fullName:"user3", age:33}];
+  // create users for different keys
+  const [users, setUsers] = useState([]);
+
+  // fetch user data FUNCTION from API
+  const fetchUserData = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json()) // object to json
+      .then((json) => setUsers(json)); // json data set to users
+  };
+
+  // render when app loads
+  useEffect(() => {
+    fetchUserData();
+  }, []);
 
   return (
     <div className="bg-slate-800/90 h-screen">
       <HeaderApp />
       <div className="mx-20 text-xs">
-      <p className="bg-yellow-100 text-center my-3">DO THIS I GUESS</p>
-        <p className="bg-orange-500 text-white my-3 line-through">1- figure this out somewhat</p>
-        <p className="bg-yellow-300/90 my-3">2- use https://jsonplaceholder.typicode.com/ for free api list</p> 
-        <p className="bg-yellow-300/90 my-3">3- fetch from API</p>
-        <p className="bg-yellow-300/90 my-3">4- render from API using useState and useEffect</p>
-        <p className="bg-yellow-300/90 my-3">5- create button for display a part of data from USER API rendered</p>
+        <p className="bg-yellow-100 text-center my-3">DO THIS I GUESS</p>
+        <p className="bg-orange-500 text-white my-3 line-through">
+          1- figure this out somewhat
+        </p>
+        <p className="bg-orange-500 text-white my-3 line-through">
+          2- use https://jsonplaceholder.typicode.com/ for free api list
+        </p>
+        <p className="bg-orange-500 text-white my-3 line-through">
+          3- fetch from API
+        </p>
+        <p className="bg-orange-500 text-white my-3 line-through">
+          4- render from API using useState and useEffect
+        </p>
+        <p className="bg-yellow-300/90 my-3">
+          5- create button for display a part of data from USER API rendered
+        </p>
         <p className="bg-yellow-300/90 my-3">6- onClick button show data</p>
-        <p className="bg-yellow-300/90 my-3">7- onClick also can hide data (useState) for show/hide</p>  
+        <p className="bg-yellow-300/90 my-3">
+          7- onClick also can hide data (useState) for show/hide
+        </p>
       </div>
 
-      <div>
-        
+      <div className="bg-blue-300/70 p-10">
+      <p className="py-4 font-bold italic text-white bg-blue-700/80 mb-4">
+          this is the API Render
+        </p>
+        {/* Render API to UI */}
+        <ol>
+          {users
+          // limit to 3 users easier to see
+          .filter((user) => (user.id <= 3)) 
+          // map each user to list items
+          .map((user) => ( 
+            <div key={user.id}>
+              <li>{user.name}</li>
+              <li>{user.address.zipcode}</li>
+            </div>
+          ))}
+        </ol>
+
+        <p></p>
       </div>
 
       <div className="text-center text-white bg-slate-800">
-        <p className="py-4 font-bold italic bg-slate-600">this is the normal array destructure mapping</p>
-        {contactList.map((contact,index)=>(
+        <p className="py-4 font-bold italic bg-slate-600">
+          this is the normal array destructure mapping
+        </p>
+        {contactList.map((contact, index) => (
           <div key={index}>
             <p>-- start --</p>
             <p>{contact.fullName}</p>
@@ -37,9 +88,33 @@ function App() {
       </div>
 
       <div className="py-20 bg-black"></div>
-
     </div>
   );
 }
 
 export default App;
+
+// Data structure of each API User
+// {
+//   "id": 1,
+//   "name": "Leanne Graham",
+//   "username": "Bret",
+//   "email": "Sincere@april.biz",
+//   "address": {
+//     "street": "Kulas Light",
+//     "suite": "Apt. 556",
+//     "city": "Gwenborough",
+//     "zipcode": "92998-3874",
+//     "geo": {
+//       "lat": "-37.3159",
+//       "lng": "81.1496"
+//     }
+//   },
+//   "phone": "1-770-736-8031 x56442",
+//   "website": "hildegard.org",
+//   "company": {
+//     "name": "Romaguera-Crona",
+//     "catchPhrase": "Multi-layered client-server neural-net",
+//     "bs": "harness real-time e-markets"
+//   }
+// }
